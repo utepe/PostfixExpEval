aac96edbb212b51635762880dee537adb98d0abd
Uygur Tepe <uygur.t10@gmail.com>
2020-06-12 10:50:01 -0400

    Main throws IOException

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         File file = new File("testCases.txt");
         BufferedWriter bw = new BufferedWriter(new FileWriter(new File("solutions.txt")));
 
